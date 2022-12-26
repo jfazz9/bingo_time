f5c42a53dd28b301e0d709e5c4018483d18158fe
jfazz9 <jftennis@gmail.com>
2022-12-26 22:43:51 +0000

    adding while loop and other conditions

--- go_bingo.py
+++ go_bingo.py
@@ -9,9 +9,38 @@ class Game:
 
 if __name__ == '__main__':
     bingo = Bingo()
-    print(bingo.all_numbers)
-    bingo.add_number(2)
-    bingo.add_number(9)
-    bingo.check_current_game()
-    print(bingo.check_number(9))
-    print(bingo.check_number(21))
+    playing = True
+    while playing:
+        print('add the number')
+        try:
+            number = int(input())
+            bingo.add_number(number)
+            print('1 for checking a number\n'
+                  '2 for checking numbers remaining\n'
+                  '3 to check numbers called\n'
+                  '4 to create a new game \n'
+                  '0 to continue')
+            decision = int(input())
+            if decision == 1:
+                checking = True
+                while checking:
+                    print('what number do you want to check? ')
+                    check_numb = int(input())
+                    print(bingo.check_number(check_numb))
+                    print('check another number? type "y" or "Y" to continue')
+                    again = input()
+                    if again in ['y', 'Y']:
+                        continue
+                    else:
+                        checking = False
+
+            elif decision == 2:
+                print(bingo.check_current_game())
+
+            elif decision == 3:
+                print(bingo.check_current_game())
+            else:
+                continue
+
+        except TypeError:
+            print('please enter a number')
