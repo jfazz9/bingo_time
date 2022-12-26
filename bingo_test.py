1b75784e24693720e22e40fb6bb4e121bd246ec2
jfazz9 <jftennis@gmail.com>
2022-12-26 19:55:46 +0000

    adjusting the test

--- bingo_test.py
+++ bingo_test.py
@@ -1,20 +1,40 @@
 import unittest
 from bingo_game import Bingo
+import random
 
 
 class TestBingo(unittest.TestCase):
+    bingo = Bingo()
+    bingo_numb = []
+    bingo_id = []
+
     def test_add_number(self):
-        bingo.add_number(9)
-        self.assertEqual(bingo.bingo_numbers[0], 9)
+        for i in range(1,5):
+            number = random.choice(bingo.all_numbers)
+            self.bingo_numb.append(number)
+            total = self.bingo.add_number(number)
+            print(total)
+            self.assertIsNotNone(total)
+            self.bingo_id.append(total)
+        print(f'bingo_numb length {len(self.bingo_numb)}')
+        print(self.bingo_numb)
+        print(f'bingo_id length {len(self.bingo_id)}')
+        print(self.bingo_id)
+
 
     def test_check_number(self):
-        bingo.add_number(9)
-        self.assertEqual(bingo.check_number(9), True)
-        self.assertEqual(bingo.check_number(9), )
+        self.assertEqual(self.bingo_numb in self.bingo_id, True)
 
-    def test_adding
 
+    # def test_adding(self):
+    #     self.assertEqual(2 in bingo.all_numbers, True)
+    #     self.assertEqual(9 in bingo.all_numbers, False)
+    #     self.assertEqual(9 in bingo.bingo_numbers, True)
+    #     self.assertEqual(2 in bingo.bingo_numbers, False)
+
+    # def test_new_game(self):
+    #     bingo.new_game()
+    #     self.assertEqual()
 
 if __name__ == '__main__':
-    bingo = Bingo()
     unittest.main()
